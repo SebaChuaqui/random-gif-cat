6cd86019d5b99618ea0fa22c321e161cba00f455
SebaChuaqui <schuaquicano@gmail.com>
2022-06-19 11:23:26 -0300

    Gatos

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <randomgifcat></randomgifcat>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import RandomGifCat from "./components/RandomGifComponent.vue"
 export default {
-  name: 'App',
+  name: 'component-name',
+
+  data: function () {
+    return {
+      message: 'Random Gif Cat',
+    }
+  },
+
+  methods: {
+
+  },
   components: {
-    HelloWorld
-  }
+    'randomgifcat': RandomGifCat,
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Montserrat', sans-serif;
 }
 </style>
